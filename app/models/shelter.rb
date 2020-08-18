441304ba81aa0b9eff103c1e5f243a3dbb6013e8
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-17 18:29:28 -0600

    Create shelter and pet model specs and models.

---
+++ app/models/shelter.rb
@@ -0,0 +1,9 @@
+class Shelter < ApplicationRecord
+  has_many :pets
+
+  validates_presence_of :name,
+                        :address,
+                        :city,
+                        :state,
+                        :zip
+end
