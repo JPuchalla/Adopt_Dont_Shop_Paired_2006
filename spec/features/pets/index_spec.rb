7201594f049a5c0e0f2d310de0d2cd40e3134cd2
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-20 14:37:30 -0600

    Complete US15 & US16; Pet update and delete from Pets Index Page.

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -24,5 +24,47 @@ RSpec.describe "Pets index page" do
       expect(page).to have_content("#{@pet2.sex}")
       expect(page).to have_content("#{@shelter2.name}")
     end
+
+    it "I see an edit pet info link next to each pet, which takes me to the edit form." do
+      visit '/pets'
+      within "#pet-#{@pet1.id}" do
+        expect(page).to have_content("#{@pet1.name}")
+        expect(page).to have_link("Edit Pet Info")
+
+        click_link "Edit Pet Info"
+      end
+      expect(current_path).to eq("/pets/#{@pet1.id}/edit")
+
+      visit '/pets'
+      within "#pet-#{@pet2.id}" do
+        expect(page).to have_content("#{@pet2.name}")
+        expect(page).to have_link("Edit Pet Info")
+
+        click_link "Edit Pet Info"
+      end
+      expect(current_path).to eq("/pets/#{@pet2.id}/edit")
+    end
+
+    it "I see an edit pet info link next to each pet, which takes me to the edit form." do
+      visit '/pets'
+      within "#pet-#{@pet1.id}" do
+        expect(page).to have_content("#{@pet1.name}")
+        expect(page).to have_link("Delete Pet")
+
+        click_link "Delete Pet"
+      end
+      expect(current_path).to eq("/pets")
+      expect(page).to_not have_content("#{@pet1.name}")
+
+      visit '/pets'
+      within "#pet-#{@pet2.id}" do
+        expect(page).to have_content("#{@pet2.name}")
+        expect(page).to have_link("Edit Pet Info")
+
+        click_link "Edit Pet Info"
+      end
+      expect(current_path).to eq("/pets/#{@pet2.id}/edit")
+      expect(page).to_not have_content("#{@pet2.name}")
+    end
   end
 end
