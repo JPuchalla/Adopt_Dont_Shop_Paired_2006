2c206e425e5795a414b3ece1e6223f5db616ccdb
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-20 15:45:36 -0600

    Complete US19 & US20; Pet index and Shelter index links in nav bar.

--- spec/features/site/nav_bar_spec.rb
+++ spec/features/site/nav_bar_spec.rb
@@ -1,11 +1,10 @@
-# require 'rails_helper'
-# describe "layouts/application.html.erb" do
-#   it "should have navbar links to shelters and pets index" do
-#     visit "/"
-#     within(".navbar") do
-#       expect(page).to have_link("Home Page", href: "/")
-#       expect(page).to have_link("Pet Index", href: "/pets")
-#       expect(page).to have_link("Shelter Index", href: "/shelters")
-#     end
-#   end
-# end
+require 'rails_helper'
+describe "layouts/application.html.erb" do
+  it "should have navbar links to shelters and pets index" do
+    visit "/shelters"
+    within(".navbar") do
+      expect(page).to have_link("Pet Index", href: "/pets")
+      expect(page).to have_link("Shelter Index", href: "/shelters")
+    end
+  end
+end
