56ef95aa9fb392fd86ce3356c78ded3b92b5a24a
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-20 14:23:28 -0600

    Complete US13 & US14; Shelter update and shelter delete from Shelter Index Page.

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -13,5 +13,47 @@ RSpec.describe "Shelters Index Page" do
       expect(page).to have_content("#{@shelter1.name}")
       expect(page).to have_content("#{@shelter2.name}")
     end
+
+    it "I see an update shelter link next to each shelter, which takes me to the edit form." do
+      visit '/shelters'
+      within "#shelter-#{@shelter1.id}" do
+        expect(page).to have_content("#{@shelter1.name}")
+        expect(page).to have_link("Update Shelter")
+
+        click_link "Update Shelter"
+      end
+      expect(current_path).to eq("/shelters/#{@shelter1.id}/edit")
+
+      visit '/shelters'
+      within "#shelter-#{@shelter2.id}" do
+        expect(page).to have_content("#{@shelter2.name}")
+        expect(page).to have_link("Update Shelter")
+
+        click_link "Update Shelter"
+      end
+      expect(current_path).to eq("/shelters/#{@shelter2.id}/edit")
+    end
+
+    it "I see a delete shelter link next to each shelter." do
+      visit '/shelters'
+      within "#shelter-#{@shelter1.id}" do
+        expect(page).to have_content("#{@shelter1.name}")
+        expect(page).to have_link("Delete Shelter")
+
+        click_link "Delete Shelter"
+      end
+      expect(current_path).to eq("/shelters")
+      expect(page).to_not have_content("#{@shelter1.name}")
+
+      visit '/shelters'
+      within "#shelter-#{@shelter2.id}" do
+        expect(page).to have_content("#{@shelter2.name}")
+        expect(page).to have_link("Delete Shelter")
+
+        click_link "Delete Shelter"
+      end
+      expect(current_path).to eq("/shelters")
+      expect(page).to_not have_content("#{@shelter2.name}")
+    end
   end
 end
