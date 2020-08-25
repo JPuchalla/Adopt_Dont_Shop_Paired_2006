2dd4238e340419adb707397f64b00cd2793e5c08
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-08-25 16:02:30 -0600

    created review table and added one to many relationship

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :shelter
+
+  validates_presence_of :title,
+                        :rating,
+                        :content,
+                        :image
+
+end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper.rb"
 RSpec.describe Shelter, type: :model do
   describe "relationships" do
     it {should have_many :pets}
+    it {should have_many :reviews}
   end
 
   describe "validations" do
