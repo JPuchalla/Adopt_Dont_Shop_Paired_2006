2f8e9577da966db0fd2a9415928306164b1a0267
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-27 16:39:44 -0600

    Create home page and add link to nav bar.

--- spec/features/site/nav_bar_spec.rb
+++ spec/features/site/nav_bar_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 describe "layouts/application.html.erb" do
-  it "should have navbar links to shelters and pets index" do
+  xit "should have navbar links to home, shelters, and pets." do
     visit "/shelters"
     within(".navbar") do
+      expect(page).to have_link("Home", href: "/")
       expect(page).to have_link("Pet Index", href: "/pets")
       expect(page).to have_link("Shelter Index", href: "/shelters")
     end
