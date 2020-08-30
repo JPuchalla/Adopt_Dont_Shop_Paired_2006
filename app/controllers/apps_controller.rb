85884ffb1753a05471d0fe7fb1c590fef1c55380
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-30 13:15:26 -0600

    US17 Complete; Incomplete application for a Pet

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -5,19 +5,24 @@ class AppsController < ApplicationController
 
   def create
     selected_pets = params.keys.find_all { |key| key.include?("select-") }
-    application = App.create(applications_params)
-    application.pets << selected_pets.map { |pet| Pet.find(params[pet].to_i) }
-    application.pets.each do |pet|
-      pet_app = PetApp.create(pet_id: pet.id, app_id: application.id )
-      favorites.remove_pet(pet.id)
+    application = App.create(application_params)
+    if application.save
+      application.pets << selected_pets.map { |pet| Pet.find(params[pet].to_i) }
+      application.pets.each do |pet|
+        PetApp.create(pet_id: pet.id, app_id: application.id )
+        favorites.remove_pet(pet.id)
+      end
+      redirect_to "/favorites"
+      flash[:notice] = "Application successfully submitted!"
+    else
+      redirect_to "/apps/new"
+      flash[:notice] = "Please ensure all form fields completed for submission."
     end
-    redirect_to "/favorites"
-    flash[:notice] = "Application successfully submitted!"
   end
 
   private
 
-  def applications_params
+  def application_params
     params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
   end
 end
