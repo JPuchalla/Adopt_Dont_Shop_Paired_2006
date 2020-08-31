433411ebcca58b86e948ea7ad223c516b9c94063
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-31 14:54:45 -0600

    Complete US20; Pet Applications Index Page

--- app/controllers/pet_apps_controller.rb
+++ app/controllers/pet_apps_controller.rb
@@ -1,3 +1,8 @@
 class PetAppsController < ApplicationController
 
+  def index
+    @pet = Pet.find(params[:id])
+    pet_apps = PetApp.where(pet_id: params[:id])
+    @current_applicants = pet_apps.map {|app| App.find(app.app_id)}
+  end
 end
