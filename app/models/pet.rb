63f0c5e4bc2309d84f8ab62d89dea4dca7a8e059
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-01 16:40:05 -0600

    Resolve relationship bug for pet moddel. Add Pet status change methods.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,20 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+  has_many :pet_apps
+  has_many :apps, through: :pet_apps
 
   validates_presence_of :image,
                         :name,
                         :age,
                         :sex
+
+  def status_to_pending
+   self.status = "Pending"
+   self.save
+  end
+
+  def status_to_adoptable
+   self.status = "Adoptable"
+   self.save
+  end
 end
